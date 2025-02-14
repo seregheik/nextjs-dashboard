4efd8ae3c7c50280472acbb8a23602b99f0720f0
Osasere Ikponmwosa <edenosas1@gmail.com>
2025-02-14 13:01:17 +0100

    corrected the font import

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+    return (
+        <div>Customers</div>
+    )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+    return (
+        <div>Invoices</div>
+    )
+}
+
+export default page
